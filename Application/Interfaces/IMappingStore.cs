7a5259c475e4b43b474976573fdc17a8dced089d
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-06-29 16:30:40 +0500

    feat: add IMappingStore

---
+++ Application/Interfaces/IMappingStore.cs
@@ -0,0 +1,11 @@
+using EdgarBot.Application.Models;
+
+namespace EdgarBot.Application.Interfaces
+{
+    public interface IMappingStore
+    {
+        void Add(int adminMessageId, ForwardedMessageInfo info);
+        bool TryGet(int adminMessageId, out ForwardedMessageInfo info);
+        void Remove(int adminMessageId);
+    }
+}
