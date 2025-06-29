45015c2ce363cce03506f0ca95106e22a96d9011
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-06-29 16:23:28 +0500

    feat: add ForwardedMessageInfo

---
+++ Application/Models/ForwardedMessageInfo.cs
@@ -0,0 +1,11 @@
+namespace EdgarBot.Application.Models
+{
+    public class ForwardedMessageInfo
+    {
+        public int AdminMessageId { get; set; }
+
+        public long UserId { get; set; }
+
+        public int UserMessageId { get; set; }
+    }
+}
