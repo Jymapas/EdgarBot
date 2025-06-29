d645e1dec46850a5cf1ad3ad0c72a8a5d74f001a
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-06-29 17:52:19 +0500

    chore(Services): add ForwardingService template

--- Application/Services/ForwardingService.cs
+++ Application/Services/ForwardingService.cs
@@ -1,6 +1,23 @@
-﻿namespace EdgarBot.Application.Services;
+﻿using EdgarBot.Application.Interfaces;
+using Telegram.Bot.Types;
 
-public class ForwardingService
+namespace EdgarBot.Application.Services;
+
+public class ForwardingService : IForwardingService
 {
-    
+    private readonly long _adminChatId;
+    private readonly IMappingStore _mappingStore;
+    private readonly IMessageSender _sender;
+
+    public ForwardingService(IMessageSender messageSender, IMappingStore mappingStore, long adminChatId)
+    {
+        _sender = messageSender;
+        _mappingStore = mappingStore;
+        _adminChatId = adminChatId;
+    }
+
+    public Task HandleUserMessageAsync(Message userMessage, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
 }
