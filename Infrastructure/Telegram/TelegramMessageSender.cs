ae64770ab92fa3ccd19e54071867c107e2e2d196
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-06-29 17:42:54 +0500

    chore: implement TelegramMessageSender

--- Infrastructure/Telegram/TelegramMessageSender.cs
+++ Infrastructure/Telegram/TelegramMessageSender.cs
@@ -1,6 +1,39 @@
-﻿namespace EdgarBot.Infrastructure.Telegram;
+﻿using EdgarBot.Application.Interfaces;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
-public class TelegramMessageSender
+namespace EdgarBot.Infrastructure.Telegram;
+
+public class TelegramMessageSender : IMessageSender
 {
-    
+    private readonly ITelegramBotClient _botClient;
+
+    public TelegramMessageSender(ITelegramBotClient botClient)
+    {
+        _botClient = botClient;
+    }
+
+    public async Task<int> SendTextMessageAsync(long chatId, string text, ReplyParameters? replyParameters = null, CancellationToken cancellationToken = default)
+    {
+        var msg = await _botClient.SendMessage(
+            chatId: chatId,
+            text: text,
+            replyParameters: replyParameters,
+            parseMode: ParseMode.Markdown,
+            cancellationToken: cancellationToken);
+
+        return msg.MessageId;
+    }
+
+    public async Task<int> CopyMessageAsync(long toChatId, long fromChatId, int messageId, CancellationToken cancellationToken = default)
+    {
+        var msg = await _botClient.CopyMessage(
+            chatId: toChatId,
+            fromChatId: fromChatId,
+            messageId: messageId,
+            cancellationToken: cancellationToken);
+
+        return msg;
+    }
 }
