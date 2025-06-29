1f53937ac9880406121559dfd8fdc12a437423cc
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-06-29 23:15:18 +0500

    chore(Presentation): add UpdateHandler

---
+++ Presentation/Telegram/EdgarUpdateHandler.cs
@@ -0,0 +1,18 @@
+﻿using EdgarBot.Presentation;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+public class EdgarUpdateHandler(UpdateHandler handler) : IUpdateHandler
+{
+    public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        await handler.HandleUpdateAsync(update, cancellationToken);
+    }
+
+    public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Polling error: {exception}");
+        return Task.CompletedTask;
+    }
+}
