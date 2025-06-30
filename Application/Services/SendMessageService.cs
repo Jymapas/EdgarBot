7f966ae8811aaae0b14e190e5e0c381b6d096cb3
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-07-01 01:50:00 +0500

    feat(Services): #2: implement SendMessageAsync

--- Application/Services/SendMessageService.cs
+++ Application/Services/SendMessageService.cs
@@ -4,10 +4,15 @@ namespace EdgarBot.Application.Services;
 
 public class SendMessageService(IMessageSender messageSender) : ISendMessageService
 {
-    private const string Text = "Вы были заблокированы администраторами.\nУзнать новости фестиваля вы можете в канале @Nevermorequestions";
+    private const string BanText = "Вы были заблокированы администраторами.\nУзнать новости фестиваля вы можете в канале @Nevermorequestions";
 
     public Task SendBannedInfoAsync(long userId, CancellationToken cancellationToken)
     {
-        return messageSender.SendTextMessageAsync(userId, Text, cancellationToken);
+        return messageSender.SendTextMessageAsync(userId, BanText, cancellationToken);
+    }
+
+    public Task SendMessageAsync(long chatId, string text, CancellationToken cancellationToken)
+    {
+        return messageSender.SendTextMessageAsync(chatId, text, cancellationToken);
     }
 }
