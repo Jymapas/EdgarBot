6c69e8f04c4ed66ec5ea1241065b14a868b70b6a
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-07-01 00:04:12 +0500

    feat(Infrastructure): #2: create SQLiteBanListStore template

---
+++ Infrastructure/Persistence/SQLiteBanListStore.cs
@@ -0,0 +1,25 @@
+﻿using EdgarBot.Application.Interfaces;
+using EdgarBot.Application.Models;
+using Microsoft.Data.Sqlite;
+
+namespace EdgarBot.Infrastructure.Persistence;
+
+public class SQLiteBanListStore : IBanListStore
+{
+    public void Ban(long userId, string name)
+    {
+        throw new NotImplementedException();
+    }
+    public void Unban(long userId)
+    {
+        throw new NotImplementedException();
+    }
+    public bool IsBanned(long userId)
+    {
+        throw new NotImplementedException();
+    }
+    public IEnumerable<BannedUserInfo> GetBannedUsers()
+    {
+        throw new NotImplementedException();
+    }
+}
