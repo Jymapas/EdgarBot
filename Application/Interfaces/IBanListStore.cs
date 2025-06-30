c6611cbe1bffd6e91546de5082a68af164b72678
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-06-30 23:40:18 +0500

    feat(Application): #2: create IBanListStore

---
+++ Application/Interfaces/IBanListStore.cs
@@ -0,0 +1,9 @@
+﻿namespace EdgarBot.Application.Interfaces;
+
+public interface IBanListStore
+{
+    void Ban(long userId);
+    void Unban(long userId);
+    bool IsBanned(long userId);
+    IEnumerable<long> GetBannedUsers();
+}
