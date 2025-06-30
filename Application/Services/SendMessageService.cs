1df635fae5c2e4271ed310c86e6a06b82cb9cc93
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-07-01 00:44:37 +0500

    feat(Application): #2: create SendMessageService

---
+++ Application/Services/SendMessageService.cs
@@ -0,0 +1,13 @@
+﻿using EdgarBot.Application.Interfaces;
+
+namespace EdgarBot.Application.Services;
+
+public class SendMessageService(IMessageSender messageSender) : ISendMessageService
+{
+    private const string Text = "Вы были заблокированы администраторами.\nУзнать новости фестиваля вы можете в канале @Nevermorequestions";
+
+    public Task SendBannedInfoAsync(long userId, CancellationToken cancellationToken)
+    {
+        return messageSender.SendTextMessageAsync(userId, Text, cancellationToken);
+    }
+}
