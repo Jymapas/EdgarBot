3ade9174b1a88e25093d2e0b8d9c902b9097a264
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-07-01 22:49:07 +0500

    chore(ForwardedMessageInfo): #2: add DateTime ForwardedAt field

--- Application/Models/ForwardedMessageInfo.cs
+++ Application/Models/ForwardedMessageInfo.cs
@@ -6,4 +6,5 @@ public class ForwardedMessageInfo
     public long UserId { get; set; }
     public string UserName { get; set; }
     public int UserMessageId { get; set; }
+    public DateTime ForwardedAt { get; set; }
 }
