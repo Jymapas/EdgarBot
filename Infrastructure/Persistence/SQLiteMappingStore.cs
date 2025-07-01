2ea019d4674052bf844f90d2ba9d0d3aa9d24c8a
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2025-07-01 22:51:14 +0500

    chore(Persistence): #2: add template SQLiteMappingStore

---
+++ Infrastructure/Persistence/SQLiteMappingStore.cs
@@ -0,0 +1,20 @@
+﻿using EdgarBot.Application.Interfaces;
+using EdgarBot.Application.Models;
+
+namespace EdgarBot.Infrastructure.Persistence;
+
+public class SQLiteMappingStore : IMappingStore
+{
+    public void Add(int adminMessageId, ForwardedMessageInfo info)
+    {
+        throw new NotImplementedException();
+    }
+    public bool TryGet(int adminMessageId, out ForwardedMessageInfo info)
+    {
+        throw new NotImplementedException();
+    }
+    public void Remove(int adminMessageId)
+    {
+        throw new NotImplementedException();
+    }
+}
