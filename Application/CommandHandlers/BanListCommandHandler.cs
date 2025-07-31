6350a4400f2ea79d5d6319b38cc9169adf4df97d
Jymapas <jymapas@yandex.ru>
2025-07-31 16:35:35 +0500

    feat(CommandHandlers): #2: add BanListCommandHandler

---
+++ Application/CommandHandlers/BanListCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using EdgarBot.Application.Helpers;
+using EdgarBot.Application.Interfaces;
+using Telegram.Bot.Types;
+
+namespace EdgarBot.Application.CommandHandlers;
+
+public class BanListCommandHandler(IBanListStore banListStore, ISendMessageService sendMessageService, IMappingStore mappingStore, long adminChatId) : ICommandHandler
+{
+    public async Task<bool> TryHandleAsync(Message message, CancellationToken cancellationToken)
+    {
+        var text = message.Text?.Trim();
+        if (message.Chat.Id != adminChatId 
+            || !CommandHelper.IsCommand(text, "banlist")
+            || message.ReplyToMessage == null)
+        {
+            return false;
+        }
+        
+        var bannedUsers = banListStore.GetBannedUsers();
+
+        if (!bannedUsers.Any())
+        {
+            await sendMessageService.SendMessageAsync(adminChatId, "Нет забаненных пользователей.", cancellationToken);
+            return true;
+        }
+
+        var sb = new StringBuilder();
+        foreach (var user in bannedUsers)
+        {
+            sb.AppendLine($"{user.Name} (`{user.UserId}`)");
+        }
+        
+        await sendMessageService.SendMessageAsync(adminChatId, sb.ToString(), cancellationToken);
+        
+        return true;
+    }
+}
